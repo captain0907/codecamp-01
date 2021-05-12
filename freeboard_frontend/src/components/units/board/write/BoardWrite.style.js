02edf78139b39a268bedf9e01c2ece1af0f3fbd1
이상권 <sk@isang-gwon-ui-MacBookPro.local>
2021-05-12 16:00:55 +0900

    readMe

---
+++ freeboard_frontend/src/components/units/board/write/BoardWrite.style.js
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div``;
+
+export const Title = styled.div``;
+
+export const WriterWrapper = styled.div``;
+
+export const InputWrapper = styled.div``;
+
+export const Label = styled.div``;
+
+export const Writer = styled.div``;
